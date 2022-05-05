9dbe70325744b932845346c129e6659fbd2d4b08
martynhorslen <2880060+MartynHorslen@users.noreply.github.com>
2022-05-05 15:04:48 +0100

    Add complete first attempt at the carousel.

--- js/banner.js
+++ js/banner.js
@@ -1,1 +1,35 @@
-console.log("Test");
+$(document).ready(function(){
+    $('#slider').slick({
+    // normal options...
+    infinite: false,
+    dots: true,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 4000,
+
+    // the magic
+    responsive: [{
+
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 1,
+            infinite: true,
+            dots: true
+        }
+
+        }, {
+
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 1,
+            dots: true
+        }
+
+        }, {
+
+        breakpoint: 300,
+        settings: "unslick" // destroys slick
+
+        }]
+        });
+  });
