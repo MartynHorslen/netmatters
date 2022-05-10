0c20a4b048985f62720e9103f1bdf8d06b46131f
martynhorslen <2880060+MartynHorslen@users.noreply.github.com>
2022-05-10 16:45:34 +0100

    Add cookie pop up and extended options.

---
+++ js/cookie.js
@@ -0,0 +1,50 @@
+$(document).ready(()=>{
+    if (!document.cookie){
+        $(".cookie-overlay").removeClass("hidden");
+    }
+});
+
+$(".right-btn").click((e)=>{
+    e.preventDefault();
+    $(".cookie-overlay").addClass("hidden");
+    const d = new Date();
+    d.setTime(d.getTime() + (1*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = expires;
+});
+
+$(".left-btn").click((e)=>{
+    e.preventDefault();
+    $(".cookie-extended-overlay").removeClass("hidden");
+});
+
+$(".btns button").click((e)=>{
+    e.preventDefault();
+    $(e.target).siblings().removeClass("default").addClass("inactive");
+    $(e.target).addClass("default").removeClass("inactive");
+});
+
+$(".table-btn").click((e)=>{
+    if ($(".table").hasClass("hidden")){
+        $(".table").removeClass("hidden");
+    } else {
+        $(".table").addClass("hidden");
+    }
+    $(e.target).addClass("hidden");
+    $(e.target).siblings().removeClass("hidden");
+});
+
+$(".right-btn-ext").click((e)=>{
+    e.preventDefault();
+    $(".cookie-overlay").addClass("hidden");
+    $(".cookie-extended-overlay").addClass("hidden");
+    const d = new Date();
+    d.setTime(d.getTime() + (1*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = expires;
+});
+
+$(".left-btn-ext").click((e)=>{
+    e.preventDefault();
+    $(".cookie-extended-overlay").addClass("hidden");
+});
