64070fcc87ac0d3df51916cdf67cc0afca62a2df
martynhorslen <2880060+MartynHorslen@users.noreply.github.com>
2022-06-14 13:49:10 +0100

    Database dump

--- database/database.sql
+++ database/database.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.9.7
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 14, 2022 at 10:01 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Host: localhost:3306
+-- Generation Time: Jun 14, 2022 at 12:48 PM
+-- Server version: 8.0.28
+-- PHP Version: 7.3.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `netmatters`
+-- Database: `martynho_netmatters`
 --
 
 -- --------------------------------------------------------
@@ -28,11 +29,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `authors` (
-  `id` int(6) NOT NULL,
+  `id` int NOT NULL,
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `authors`
@@ -50,24 +51,30 @@ INSERT INTO `authors` (`id`, `first_name`, `last_name`, `image`) VALUES
 --
 
 CREATE TABLE `contact` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `name` varchar(40) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `telephone` varchar(11) NOT NULL,
+  `telephone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `subject` varchar(100) NOT NULL,
   `message` varchar(500) NOT NULL,
-  `marketing` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `marketing` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `contact`
 --
 
 INSERT INTO `contact` (`id`, `name`, `email`, `telephone`, `subject`, `message`, `marketing`) VALUES
-(23, 'Martyn Horslen', 'martynhorslen@gmail.com', '0798424521', 'test', 'This is a test message.', 0),
 (24, 'Testy McTestalot', 'tester@te.st', '01234 5687', 'test', 'This is a test message.', 1),
-(25, 'Martyn Horslen', 'martynhorslen@gmail.com', '0798424521', 'sdghhfda', 'Robert&#039;); DROP TABLE STUDENTS; --', 1),
-(26, 'Martyn Horslen', 'martynhorslen@gmail.com', '0798424521', 'sdghhfda', 'Robert&#039;); DROP TABLE STUDENTS; --', 1);
+(27, 'Othello', 'deemonti07@gmail.com', '0333444242', 'eeejej', 'dddha', 1),
+(28, 'Othello', 'deemonti07@gmail.com', '0333444242', 'eeejej', 'dddha', 1),
+(29, 'Anna', 'blabla@gmail.com', '0777777777', 'Netmatters', 'Testing the page on Chrome', 1),
+(30, 'Anna', 'blabla@gmail.com', '0777777777', 'Netmatters', 'Test one more time', 1),
+(31, 'Jezza Jezabelle', 'Jeremy.Hanson@netmatters-scs.com', '07666666666', 'A subject', 'A message', 0),
+(33, 'Jezza Jezabelle', 'Jeremy.Hansen@netmatters-scs.com', '07666666666', 'A subject', 'A message', 0),
+(34, 'Brendan', 'brendan-boyle@netmatters-scs.com', '0891505050', 'Net Matters', 'My Net is full of holes!\r\nThis is not acceptable.', 0),
+(35, 'Martyn Horslen', 'martynhorslen@gmail.com', '07984245214', 'afdgcgh', 'agdas', 1),
+(36, 'Martyn Horslen', 'martynhorslen@gmail.com', '07984245214', '\\sfdgsga', 'asdfgaha', 0);
 
 -- --------------------------------------------------------
 
@@ -76,15 +83,15 @@ INSERT INTO `contact` (`id`, `name`, `email`, `telephone`, `subject`, `message`,
 --
 
 CREATE TABLE `posts` (
-  `id` int(10) NOT NULL,
+  `id` int NOT NULL,
   `title` varchar(50) NOT NULL,
   `description` varchar(103) NOT NULL,
   `link` varchar(100) NOT NULL,
-  `tag_id` int(6) NOT NULL,
+  `tag_id` int NOT NULL,
   `post_img` varchar(100) NOT NULL,
-  `date` datetime(6) NOT NULL DEFAULT current_timestamp(6),
-  `author_id` int(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `date` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `author_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `posts`
@@ -102,11 +109,11 @@ INSERT INTO `posts` (`id`, `title`, `description`, `link`, `tag_id`, `post_img`,
 --
 
 CREATE TABLE `tags` (
-  `id` int(6) NOT NULL,
+  `id` int NOT NULL,
   `name` varchar(50) NOT NULL,
   `link` varchar(100) NOT NULL,
   `department` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tags`
@@ -153,25 +160,25 @@ ALTER TABLE `tags`
 -- AUTO_INCREMENT for table `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tags`
 --
 ALTER TABLE `tags`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
